1d2dd9dfb97f551239f28a2a6aa3607d32002fee
Biruk Yidnekachew <byidnekachew@my.waketech.edu>
2025-02-26 13:52:24 -0500

    Freaky list added

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // Dystopian & Sci-Fi
+const contentTxt = ["Aliens Arrive", 
+    [
+        ["Hide at home",
+            [
+                ["Tell aliens to go away",
+                    [
+                        "Respond disrespectfully and die","Respectfully and live"
+                    ]
+                ],
+
+                ["Open door and die"]
+            ]
+        ], 
+    
+        ["Go outside", 
+            [
+                ["Meet friend",
+                    [
+                        ["Hang out with",
+                            ["Kill derftg become King"]
+                        ],
+                        ["Abandon derftg",
+                            [
+                                "Steal and aliens kill","Leave cash and live"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+]
+
+function sceneUpdate(lst){
+    document.getElementById("content").textContent = lst[0]
+}
+
+sceneUpdate(contentTxt)
